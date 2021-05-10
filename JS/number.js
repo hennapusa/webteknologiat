f6e0d3d194c1cc085e24c3e1cddfa57afb72429a
Henna Pusa <Henna.Pusa@student.lab.fi>
2021-05-10 14:44:47 +0300

    uudet tehtävät

--- JS/number.js
+++ JS/number.js
@@ -1,15 +1,17 @@
 let numbersUrl = "http://numbersapi.com/random/trivia";
+let businessIdUrl = "https://avoindata.prh.fi/bis/v1/0108023-3";
 let counter = 0;
 
 $(function () {
 
     $("#gif").hide();
+    $("#accordion").accordion({collapsible: true});
 
 });
 
 $("#btn").click(function () {
     $("#gif").show();
-    $.ajax({
+   $.ajax({
         type: "GET",
         url: numbersUrl,
         dataType: "text",
@@ -18,8 +20,25 @@ $("#btn").click(function () {
             $("#gif").hide();
             $("#numberInfo").append("<br><br><i>" + response + "</i>");
 
+           $("#accordion").append("<h4>joke</h4><div>" + response + "</div>");
+            $("#accordion").accordion("refresh");
+
         }
     });
+
+  /*  $.ajax({
+        type: "GET",
+        url: businessIdUrl,
+        dataType: "json",
+        success: function (response) {
+            console.log(response.results[0].name);
+            $("#gif").hide();
+            $("#numberInfo").append("<br><br><i>" + response.results[0].businessId+" - "+response.results[0].name + "</i>");
+ 
+        }
+        
+    }); */
+
 
     counter = counter + 1;
     $("#counterCol").text("Tehdyt haut " + counter + " kpl");
